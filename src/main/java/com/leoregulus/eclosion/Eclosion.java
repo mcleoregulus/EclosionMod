ce5906f14c3e0fef6dad90e49ed397ba0bc82065
LeoRegulus <mcleoregulus@outlook.com>
2026-04-06 22:42:05 +0800

    register item

--- src/main/java/com/leoregulus/eclosion/Eclosion.java
+++ src/main/java/com/leoregulus/eclosion/Eclosion.java
@@ -1,7 +1,103 @@
+//package com.leoregulus.eclosion;
+//
+//import com.leoregulus.eclosion.item.ModItems;
+//import com.mojang.logging.LogUtils;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.world.item.CreativeModeTabs;
+//import net.minecraft.world.level.block.Blocks;
+//import net.minecraftforge.api.distmarker.Dist;
+//import net.minecraftforge.common.MinecraftForge;
+//import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+//import net.minecraftforge.event.server.ServerStartingEvent;
+//import net.minecraftforge.eventbus.api.IEventBus;
+//import net.minecraftforge.eventbus.api.SubscribeEvent;
+//import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.fml.config.ModConfig;
+//import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+//import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+//import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+//import net.minecraftforge.registries.ForgeRegistries;
+//import org.slf4j.Logger;
+//
+//// The value here should match an entry in the META-INF/mods.toml file
+//@Mod(Eclosion.MOD_ID)
+//public class Eclosion
+//{
+//    // Define mod id in a common place for everything to reference
+//    public static final String MOD_ID = "eclosion";
+//    // Directly reference a slf4j logger
+//    private static final Logger LOGGER = LogUtils.getLogger();
+//
+//    public Eclosion(FMLJavaModLoadingContext context)
+//    {
+//        IEventBus modEventBus = context.getModEventBus();
+//
+//        // Register the commonSetup method for modloading
+//        modEventBus.addListener(this::commonSetup);
+//        ModItems.register(modEventBus);
+//
+//        // Register ourselves for server and other game events we are interested in
+//        MinecraftForge.EVENT_BUS.register(this);
+//
+//        // Register the item to a creative tab
+//        modEventBus.addListener(this::addCreative);
+//
+//        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+//        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+//    }
+//
+//    private void commonSetup(final FMLCommonSetupEvent event)
+//    {
+//        // Some common setup code
+//        LOGGER.info("HELLO FROM COMMON SETUP");
+//
+//        if (Config.logDirtBlock)
+//            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+//
+//        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+//
+//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+//    }
+//
+//    // Add the example block item to the building blocks tab
+//    private void addCreative(BuildCreativeModeTabContentsEvent event)
+//    {
+//        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+//            event.accept(ModItems.ICE_ETHER);
+//            event.accept(ModItems.RAW_ICE_ETHER);
+//            event.accept(ModItems.CARDBOARD);
+//        }
+//    }
+//
+//    // You can use SubscribeEvent and let the Event Bus discover methods to call
+//    @SubscribeEvent
+//    public void onServerStarting(ServerStartingEvent event)
+//    {
+//        // Do something when the server starts
+//        LOGGER.info("HELLO from server starting");
+//    }
+//
+//    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+//    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+//    public static class ClientModEvents
+//    {
+//        @SubscribeEvent
+//        public static void onClientSetup(FMLClientSetupEvent event)
+//        {
+//            // Some client setup code
+//            LOGGER.info("HELLO FROM CLIENT SETUP");
+//            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+//        }
+//    }
+//}
+
+
 package com.leoregulus.eclosion;
 
+import com.leoregulus.eclosion.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -90,8 +186,11 @@ public class Eclosion
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-//        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-//            event.accept(EXAMPLE_BLOCK_ITEM);
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ICE_ETHER);
+            event.accept(ModItems.RAW_ICE_ETHER);
+            event.accept(ModItems.CARDBOARD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/leoregulus/eclosion/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.leoregulus.eclosion.item;
+
+import com.leoregulus.eclosion.Eclosion;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Eclosion.MOD_ID);
+
+    public static final RegistryObject<Item> ICE_ETHER =
+            ITEMS.register("ice_ether", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ICE_ETHER =
+            ITEMS.register("raw_ice_ether", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CARDBOARD =
+            ITEMS.register("material/cardboard", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
